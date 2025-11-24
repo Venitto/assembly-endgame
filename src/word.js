4ae52d15d7a4e9752f631128ddae11d5829002b3
Venitto Chettiar <chettiarvenitto@gmail.com>
2025-11-24 19:32:52 +0530

    New Project

--- src/word.js
+++ src/word.js
@@ -4,10 +4,7 @@ export async function fetchRandomWord() {
     try {
         const response = await fetch(api_url);
         const data = await response.json();
-        console.log(data);
         const word = data[0];
-        console.log(word)
-        console.log(typeof(word));
         return word;
     } catch (error) {
         console.error("Error fetching random word:", error);
